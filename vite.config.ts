cb4b95f5bbc3ecf08ce41793edee916e2db64741
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev2005@gmail.com>
2026-06-27 12:33:38 +0500

    config: change dev server port from 5174 to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,6 @@ export default defineConfig({
       },
     }),
   ],
-  server: { host: true, port: 5174 },
-  preview: { host: true, port: 5174 },
+  server: { host: true, port: 3000 },
+  preview: { host: true, port: 3000 },
 });
